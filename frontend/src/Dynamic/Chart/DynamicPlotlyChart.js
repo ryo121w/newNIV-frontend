267fb29ddb3a7cc16149a43d008de94360279c90
ryo121w <ryoutarou9121@icloud.com>
2023-11-05 12:13:44 +0900

    commit

--- frontend/src/Dynamic/Chart/DynamicPlotlyChart.js
+++ frontend/src/Dynamic/Chart/DynamicPlotlyChart.js
@@ -1,12 +1,22 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { SketchPicker } from 'react-color';
 import Plotly from 'plotly.js';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';  // スタイルもインポートする必要があります。
+
 import createPlotlyComponent from 'react-plotly.js/factory';
 import * as XLSX from 'xlsx';
 import styles from '../css/DynamicChart.module.css';
 import dynamicButtonStyles from '../css/DynamicButton.module.css';
 
+// =============================================================================================
+
 const Plot = createPlotlyComponent(Plotly);
 
+// =============================================================================================
+
+
+// =============================================================================================
 const findPeaksAndValleys = (dataset) => {
     const peaks = [];
     const yData = dataset.y;
@@ -18,24 +28,54 @@ const findPeaksAndValleys = (dataset) => {
     }
     return peaks;
 };
+// =============================================================================================
 
+
+// =============================================================================================
 const calculateMean = (values) => {
     return values.reduce((a, b) => a + b) / values.length;
 };
+// =============================================================================================
 
+
+// =============================================================================================
 
 const calculateVariance = (values) => {
     const mean = calculateMean(values);
     return values.reduce((a, b) => a + (b - mean) ** 2, 0) / values.length;
 };
+// =============================================================================================
 
+
+
+// =============================================================================================
 const DynamicPlotlyChart = () => {
+
     const [data, setData] = useState([]);
     const [showPeaks, setShowPeaks] = useState(false);
     const [animationState, setAnimationState] = useState('initial');
     const inputRef = useRef(null);
     const [isXAxisReversed, setXAxisReversed] = useState(false);
     const [revision, setRevision] = useState(0);
+    const [xAxisTickFormat, setXAxisTickFormat] = useState('.0f');
+    const [yAxisTickFormat, setYAxisTickFormat] = useState('.5f');
+    const [concentrations, setConcentrations] = useState([]);
+    const [originalJson, setOriginalJson] = useState([]);
+    const [spectrumColors, setSpectrumColors] = useState({});
+    const [xAxisLabel, setXAxisLabel] = useState('Wavelength(cm-1)');
+    const [yAxisLabel, setYAxisLabel] = useState('Absorbance');
+    const [sliderValue, setSliderValue] = useState(parseInt(yAxisTickFormat.slice(2, 3), 10));
+    const [xSliderValue, setXSliderValue] = useState(parseInt(xAxisTickFormat.slice(2, 3), 10));
+    const [concentration_headers, setConcentrationHeaders] = useState([]);
+    const [showPicker, setShowPicker] = useState(null);  // 表示するカラーピッカーを制御
+    const colorPickerRef = useRef(null);
+
+
+    // =============================================================================================
+
+    const handleColorChange = (header, color) => {
+        setSpectrumColors(prev => ({ ...prev, [header]: color }));
+    };
     // buttonがクリックされたときにinput要素をクリックする関数
     const triggerInputClick = () => {
         inputRef.current.click();
@@ -68,8 +108,11 @@ const DynamicPlotlyChart = () => {
         console.log("isXAxisReversed is now: ", isXAxisReversed);  // 追加
     }, [isXAxisReversed]);
 
+    // =============================================================================================
 
 
+
+    // =============================================================================================
     const layout = {
         dragmode: 'zoom',
         width: 900,  // グラフの幅を設定
@@ -77,7 +120,11 @@ const DynamicPlotlyChart = () => {
 
         xaxis: {
             title: {
-                text: 'Wavelength(cm-1)',
+                text: xAxisLabel, // Update this line
+                font: {
+                    family: 'Arial Black, Arial, sans-serif',  // 太字のフォントを指定
+                    size: 18  // サイズを調整
+                }
             },
             showgrid: false,
             zeroline: false,
@@ -85,23 +132,24 @@ const DynamicPlotlyChart = () => {
             autorange: isXAxisReversed ? 'reversed' : 'normal',
             linecolor: 'rgba(153, 153, 153, 0.8)',
             linewidth: 1,
-            tickformat: '.0f',
+            tickformat: xAxisTickFormat,
+
+
         },
-
-
-
-
         yaxis: {
             title: {
-                text: 'Absorbance',
+                text: yAxisLabel, // And this line
+                font: {
+                    family: 'Arial Black, Arial, sans-serif',  // 太字のフォントを指定
+                    size: 18 // サイズを調整
+                }
+
             },
             showgrid: false,
             zeroline: false,
             linecolor: 'rgba(153, 153, 153, 0.8)',
-            tickformat: '.5f',
+            tickformat: yAxisTickFormat,
         },
-
-
         showlegend: true,
         hovermode: 'closest', // Add this line for custom tooltip mode
         hoverlabel: {
@@ -120,26 +168,13 @@ const DynamicPlotlyChart = () => {
         animation: {
             duration: 1000,
             easing: 'cubic-in-out'
-
-
         }
     };
-    const config = {
-        displayModeBar: true,
-        modeBarButtonsToAdd: ['select2d', 'lasso2d'],
-        modeBarButtonsToRemove: ['zoomOut2d', 'zoomIn2d']
-    };
+    // =============================================================================================
 
 
 
-
-
-
-
-
-
-
-
+    // =============================================================================================
     useEffect(() => {
         const plotElement = plotRef.current;
         if (plotElement && typeof plotElement.on === 'function') {
@@ -187,9 +222,11 @@ const DynamicPlotlyChart = () => {
             };
         }
     }, [plotRef, animationState]);
+    // =============================================================================================
 
 
 
+    // =============================================================================================
     const handleFileChange = (e) => {
         const file = e.target.files[0];
         if (file) {
@@ -201,33 +238,81 @@ const DynamicPlotlyChart = () => {
                 const worksheet = workbook.Sheets[sheetName];
                 const json = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
 
-                const labels = json.map((row) => row[0]);
-                const concentrations = Object.keys(json[0]).slice(1);
+                // Concentrations or legend headers are assumed to be in the first row
+                const concentrations = json[0].slice(1);
+                setConcentrationHeaders(concentrations);
+
                 const dynamicColor = (index) => `hsl(${240 + (index * 20) % 120}, 100%, 50%)`;
 
-                const newDataset = concentrations.map((concentration, index) => {
-                    return {
-                        x: labels,
-                        y: json.map((row) => row[index + 1]),
-                        type: 'scatter',
-                        mode: 'lines',
-                        name: concentration,
-                        line: {
-                            shape: 'spline',
-                            color: dynamicColor(index),
-                            width: 1
-                        }
-                    };
+                // Update spectrumColors state for the default colors
+                const initialColors = {};
+                concentrations.forEach((concentration, index) => {
+                    initialColors[concentration] = dynamicColor(index);
                 });
-                setData(newDataset);
+                setSpectrumColors(initialColors);
+
+                setOriginalJson(json);
             };
             reader.readAsBinaryString(file);
         }
-        if (animationState !== 'done') {
-            setAnimationState('animate');
-        }
     };
 
+    useEffect(() => {
+        if (originalJson.length > 0) {
+            setData(computeDataset(originalJson));
+        }
+    }, [originalJson, spectrumColors]);
+
+
+    // =============================================================================================
+
+
+    // =============================================================================================
+    useEffect(() => {
+        if (data.length > 0) {
+            const firstDataset = data[0];
+            if (firstDataset && firstDataset.x) {
+                const json = firstDataset.x.map((xValue, index) => {
+                    const row = { [xValue]: xValue };
+                    concentrations.forEach((concentration, cIndex) => {
+                        row[concentration] = data[cIndex].y[index];
+                    });
+                    return row;
+                });
+                setData(computeDataset(json));
+            }
+        }
+    }, [spectrumColors]);
+
+
+    useEffect(() => {
+        setData(computeDataset(originalJson));
+    }, [spectrumColors, originalJson]);
+    // =============================================================================================
+
+
+    // =============================================================================================
+    const computeDataset = (json) => {
+        const labels = json.map((row) => row[0]);
+        return concentration_headers.map((header, index) => {
+            return {
+                x: labels,
+                y: json.map((row) => row[index + 1]),
+                type: 'scatter',
+                mode: 'lines',
+                name: header,
+                line: {
+                    shape: 'spline',
+                    color: spectrumColors[header] || 'blue',  // spectrumColorsを使用して色を設定
+                    width: 1
+                }
+            };
+        });
+    };
+
+    // =============================================================================================
+
+    // =============================================================================================
     const handleFindPeaks = () => {
         if (showPeaks) {
             setData(data.filter((trace) => trace.name !== 'Peaks'));
@@ -263,13 +348,17 @@ const DynamicPlotlyChart = () => {
             Plotly.relayout(plotRef.current, { 'dragmode': 'zoom' });
         }
     };
+    // =============================================================================================
 
+    // =============================================================================================
     const handlePlotRelayout = (eventData, graphDiv) => {
         if (graphDiv && graphDiv._fullLayout && (eventData['xaxis.range[0]'] || eventData['yaxis.range[0]'])) {
             Plotly.relayout(graphDiv, { 'yaxis.showline': true });
         }
     };
+    // =============================================================================================
 
+    // =============================================================================================
     const handlePlotlySelected = (eventData) => {
         console.log("Complete event data:", JSON.stringify(eventData));
         if (!eventData) return;
@@ -309,6 +398,34 @@ const DynamicPlotlyChart = () => {
         console.log(`y軸平均: ${totalMean}`);
         console.log(`分散: ${totalVariance}`);
     };
+    // =============================================================================================
+
+
+    const plotlyConfig = {
+        // 他の設定（必要に応じて）
+        toImageButtonOptions: {
+            format: 'png', // 画像の形式（png, jpeg, webpなど）
+            width: 1600,   // 画像の幅
+            height: 1200,  // 画像の高さ
+            scale: 3      // 画像のスケール（解像度を向上させるための倍率）
+        }
+    };
+
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (colorPickerRef.current && !colorPickerRef.current.contains(event.target)) {
+                setShowPicker(null);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+
 
     return (
         <div className={styles['content-wrapper']}>
@@ -344,8 +461,144 @@ const DynamicPlotlyChart = () => {
                         onRelayout={(eventData, graphDiv) => handlePlotRelayout(eventData, graphDiv)}
                         onSelected={(eventData) => handlePlotlySelected(eventData)}
                         revision={revision}
+                        config={plotlyConfig}
                     />
                 )}
+
+
+                <div className={styles['settings-container']}>
+                    <div className={styles['settings-controls']}>
+                        <div className={styles['slider-container']}>
+                            <label className={styles['slider-label']}>x-axis</label>
+                            <Slider
+                                min={0}
+                                max={10}
+                                value={xSliderValue}
+                                onChange={value => {
+                                    setXSliderValue(value);
+                                    setXAxisTickFormat(`.${value}f`);
+                                }}
+                                trackStyle={{
+                                    backgroundColor: '#fff',
+                                    height: 14
+                                }}
+                                handleStyle={{
+                                    borderColor: '#CBCDCD',
+                                    borderWidth: 0.5,
+                                    height: 14,
+                                    width: 14, // 横の大きさを少し大きくして楕円の形に
+                                    marginLeft: 0, // ハンドルの半分の幅
+
+                                    marginTop: 0, // トラックの半分の高さからハンドルの半分の高さを引く
+                                    backgroundColor: '#fff',
+                                    boxShadow: 'none',
+                                    borderRadius: '10px' // 上下は7px、左右は10pxの楕円に
+                                }}
+                                railStyle={{
+                                    backgroundColor: '#ddd',
+                                    height: 14
+                                }}
+                            />
+                        </div>
+                        <div className={styles['slider-container']}>
+                            <label className={styles['slider-label']}>y-axis</label>
+                            <Slider
+                                min={0}
+                                max={10}
+                                value={sliderValue}
+                                onChange={value => {
+                                    setSliderValue(value);
+                                    setYAxisTickFormat(`.${value}f`);
+                                }}
+                                trackStyle={{
+                                    backgroundColor: '#fff',
+                                    height: 14
+                                }}
+                                handleStyle={{
+                                    borderColor: '#CBCDCD',
+                                    borderWidth: 0.5,
+                                    height: 14,
+                                    width: 14, // 横の大きさを少し大きくして楕円の形に
+                                    marginLeft: 0, // ハンドルの半分の幅
+
+                                    marginTop: 0, // トラックの半分の高さからハンドルの半分の高さを引く
+                                    backgroundColor: '#fff',
+                                    boxShadow: 'none',
+                                    borderRadius: '10px' // 上下は7px、左右は10pxの楕円に
+                                }}
+                                railStyle={{
+                                    backgroundColor: '#ddd',
+                                    height: 14
+                                }}
+                            />
+                        </div>
+                        <div className={styles['label-input']}>
+                            <label>X Label</label>
+                            <input
+                                type="text"
+                                value={xAxisLabel}
+                                onChange={e => setXAxisLabel(e.target.value)}
+                                className={styles['styled-input']}
+                            />
+                        </div>
+                        <div className={styles['label-input']}>
+                            <label>Y Label</label>
+                            <input
+                                type="text"
+                                value={yAxisLabel}
+                                onChange={e => setYAxisLabel(e.target.value)}
+                                className={styles['styled-input']}
+                            />
+                        </div>
+                    </div>
+
+                    <div className={styles['color-pickers']}>
+                        {concentration_headers.map(header => (
+                            <div key={header} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+                                {/* Color Preview */}
+                                <div
+                                    style={{
+                                        width: '20px',
+                                        height: '20px',
+                                        borderRadius: '50%',
+                                        backgroundColor: spectrumColors[header] || '#0000FF',
+                                        marginRight: '10px',
+                                        cursor: 'pointer'
+                                    }}
+                                    onClick={() => setShowPicker(prevHeader => (prevHeader === header ? null : header))}
+                                ></div>
+
+                                {showPicker === header && (
+                                    <div
+                                        ref={colorPickerRef}
+                                        tabIndex={0}
+                                        style={{
+                                            outline: "none",
+                                            zIndex: 1000,
+                                            position: 'fixed', // これを追加
+                                            top: '10px',       // これを追加
+                                            right: '10px'     // これを追加
+                                        }}
+                                    >
+                                        <SketchPicker
+                                            color={spectrumColors[header] || '#0000FF'}
+                                            onChangeComplete={color => {
+                                                handleColorChange(header, color.hex);
+                                            }}
+                                        />
+                                    </div>
+                                )}
+
+
+                                {/* Label */}
+                                <label>{header} Color</label>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+
+
             </div>
         </div>
     );
